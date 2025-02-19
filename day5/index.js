ccd1ef44bb929a07e423cbb93160bff7e463955c
suyashdhumal35 <suyashdhumal35@gmail.com>
2025-02-19 19:55:20 +0530

    day 4 CRUD operation

--- day5/index.js
+++ day5/index.js
@@ -1,10 +1,26 @@
+const fs = require('fs');
+const path = require('path');
+const dirPath = path.join(__dirname, "crud");
+const filePath = `${dirPath}/apple.txt`
 
-var http = require('http');
-const data = require('./data');
 
-http.createServer(function (req, resp) {
-    resp.writeHead(200, { 'Content-Type': 'application/json' });
-    resp.write(JSON.stringify(data))
-    resp.end()
-}).listen(8081);
-console.log('Server running at http://127.0.0.1:8081/');
+// create
+// fs.writeFileSync(filePath, 'This is a single text file')
+
+// read
+// fs.readFile(filePath, 'utf8', (err, item) => {
+//     console.log(item);
+// })
+
+// update
+// fs.appendFile(filePath, 'and file name is apple.txt', (err) => {
+//     if (!err) console.log(" file is updated");
+// })
+
+// rename
+//     fs.rename(filePath, `${dirPath}/fruit.txt`, (err) => {
+//         if (!err) console.log(" file name is updated");
+// })
+
+// delete
+// fs.unlinkSync(`${dirPath}/fruit.txt`)
