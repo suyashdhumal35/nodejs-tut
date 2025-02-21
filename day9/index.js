6b6970cf452b018076c8401835356a7bfda121af
suyashdhumal35 <suyashdhumal35@gmail.com>
2025-02-21 15:11:11 +0530

    middleware applied only on the route.get once and not on app.get

--- day9/index.js
+++ day9/index.js
@@ -2,24 +2,19 @@
 const express = require("express");
 const reqFilter = require("./middlewaare");
 const app = express()
-const route = express.Router
+
 // app.use(reqFilter)
 
-route.use(reqFilter)
 app.get('/', (req, resp) => {
     resp.send('Welcome to the Home page')
 })
 
-app.get('/users', (req, resp) => {
+app.get('/users', reqFilter, (req, resp) => {
     resp.send('Welcome to the User page')
 })
-route.get('/about', (req, resp) => {
+app.get('/about', reqFilter, (req, resp) => {
     resp.send('Welcome to the about page')
 })
-route.get('/contact', (req, resp) => {
-    resp.send('Welcome to the Contact page')
-})
-app.use('/', route)
 
 app.listen(5000)
 
