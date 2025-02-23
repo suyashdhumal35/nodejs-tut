096bc665157e1de7a01a46fb4362ab0ab3bfbe25
suyashdhumal35 <suyashdhumal35@gmail.com>
2025-02-23 13:59:11 +0530

    to insert a query in mongodb using nodejs

---
+++ day10/insert.js
@@ -0,0 +1,16 @@
+const dbConnect = require('./mongodb')
+
+const insert = async () => {
+    const db = await dbConnect();
+    const result = await db.insert(
+        [
+            { name: 'max 1', brand: 'vivo', price: 3400, category: 'mobile' },
+            { name: 'max 2', brand: 'vivo', price: 3600, category: 'mobile' },
+            { name: 'max 3', brand: 'vivo', price: 3800, category: 'mobile' },
+        ]
+    )
+    if (result.acknowledged) {
+        console.log("data inserted");
+    }
+}
+insert()
